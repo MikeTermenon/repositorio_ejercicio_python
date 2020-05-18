c1a1484d856d2f78759b8b98d4c9f809b6638207
Mike <miguelromanov9@gmail.com>
2020-05-18 02:14:13 +0200

    Tercer commit

--- repositorio_ejercicio_python/circulo.py
+++ repositorio_ejercicio_python/circulo.py
@@ -4,5 +4,10 @@ def calcular_area(r):
     area = math.pi * r**2
     return area
 
+def calcular_perimetro(r):
+    perimetro = 2 * math.pi * r
+    return perimetro
+
 r = int(input("Dame un radio: "))
 print(calcular_area(r))
+print(calcular_perimetro(r))
