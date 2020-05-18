b20a31d5fde650b005eb41d634bfec0525ecc836
Mike <miguelromanov9@gmail.com>
2020-05-18 02:19:17 +0200

    Cuarto y último commit

--- repositorio_ejercicio_python/README.md
+++ repositorio_ejercicio_python/README.md
@@ -1,1 +1,3 @@
-"# repositorio_ejercicio_python" 
+# repositorio_ejercicio_python
+
+## El programa calcula el perímetro y área de un círculo a través de un radio dado, y el volumen de una esfera con ese radio.
--- repositorio_ejercicio_python/circulo.py
+++ repositorio_ejercicio_python/circulo.py
@@ -8,6 +8,11 @@ def calcular_perimetro(r):
     perimetro = 2 * math.pi * r
     return perimetro
 
+def calcular_volumen_esfera(r):
+    volumen = (4/3) * math.pi * r**3
+    return volumen
+
 r = int(input("Dame un radio: "))
 print(calcular_area(r))
 print(calcular_perimetro(r))
+print(calcular_volumen_esfera(r))
